1902ccc478c77900811327bf0ed2187937413e0b
liuzimin <david9719717@yahoo.com>
2020-04-23 17:28:55 -0400

    Init Upload

---
+++ build/precache-manifest.ffdb29d6f7c29d29b5e14f7e7b8839cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "884e3bf2ff3e60c09e2b",
+    "url": "/static/css/main.d59a8260.chunk.css"
+  },
+  {
+    "revision": "884e3bf2ff3e60c09e2b",
+    "url": "/static/js/main.884e3bf2.chunk.js"
+  },
+  {
+    "revision": "4b942040e2234b0cc0de",
+    "url": "/static/js/1.4b942040.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9723157fed684f649ec601d0c9bb27ba",
+    "url": "/static/media/DrapeBackground.9723157f.png"
+  },
+  {
+    "revision": "e068ce2f4e48a74feb251a350d7e4d18",
+    "url": "/index.html"
+  }
+];
